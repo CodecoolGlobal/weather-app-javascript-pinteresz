32134f4e1dd124f3877a4bac539834a5d83ee434
KovaDav <kovacs.david990420@gmail.com>
2023-01-24 16:23:47 +0100

    backspace done, change still doesnt work

--- script.js
+++ script.js
@@ -2,28 +2,77 @@ import {capitals} from '/data.js';
 
 console.log(capitals[0]);  
 
+let rootElement = document.getElementById("root");
+
+let cities;
+let optionsHistory = []
+
 const loadEvent = function() {
 
-    const url = "http://api.weatherapi.com/v1/current.json?key=";
+    const url = "http://api.weatherapi.com/v1/search.json?key=";
     const apiKey = "eea065bb85d943208ab85913232401&q=";
-    let qParameter = capitals[1].city;
+    let qParameter
 
-   const fetchWeatherData = () =>{
-        try{
-          fetch(`${url}${apiKey}${qParameter}`)
-          .then(response=>response.json())
-          .then(data=>{
-            console.log(data)
-        // displayData(data)
-          })
-        }catch(error){
-          console.log(error)
-        }
-      }
+    rootElement.insertAdjacentHTML("beforeend", `<label>Choose a capital</label>`)
+    rootElement.insertAdjacentHTML("beforeend", `<input id="input" list="datalist">`)
+    rootElement.insertAdjacentHTML("beforeend", `<datalist id="datalist">`)
 
      
-      fetchWeatherData()
+  
+      const fetchWeatherData = () =>{
+          try{
+            fetch(`${url}${apiKey}${qParameter}`)
+            .then(response=>response.json())
+            .then(data=>{           
+              cities = JSON.parse(JSON.stringify(data));
+              cities.forEach(city => {
+                document.getElementById("datalist").insertAdjacentHTML("beforeend", optionElement(city))
+                });
+            })
+          }catch(error){
+            console.log(error)
+          }
+      }
 
+      
+      let optionElement = function (city){
+        //  if(!optionsHistory.includes(city.name)){
+        //    optionsHistory.push(city.name)
+           console.log(optionsHistory);
+          return `<option id="${city.name}" value="${city.name}">`
+        //  }
+      }
+      
+
+
+        document.getElementById("input").addEventListener('keyup', () => {
+        if(document.getElementById("input").value.length >= 3){
+          qParameter = document.getElementById("input").value
+          fetchWeatherData()
+        }
+      })
+      
+      
+      document.getElementById("input").addEventListener('change', (e) => {
+        console.log("asd");
+          for(let city of cities){
+            if(city.name === e.target.value){
+              console.log(city);
+            }
+          }
+          
+          document.querySelectorAll('option').forEach(option => option.remove())
+        });
+        
+        
+        addEventListener('keydown', function(event) {
+        const key = event.key;
+          if (key === "Backspace" || key === "Delete") {
+            document.querySelectorAll('option').forEach(option => option.remove())
+          }
+        });  
+
+    
 }
 
 
